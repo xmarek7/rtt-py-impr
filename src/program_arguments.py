3634554c536f7b163636ad10cf33ace37afa0b86
Patrik Vavercak <vavercak.pato@gmail.com>
2022-03-29 20:03:40 +0000

    Add program_arguments.py

---
+++ src/program_arguments.py
@@ -0,0 +1,39 @@
+import argparse
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Python implementation of Randomness Testing Toolkit"
+    )
+    parser.add_argument("--general-settings", "-g",
+                        help="Path to a JSON file containing general settings",
+                        type=str,
+                        required=True)
+    parser.add_argument("--test-settings", "-t",
+                        help="Path to a JSON file containing settings related to batteries of tests",
+                        type=str,
+                        required=True)
+    parser.add_argument("--extensions", "-e",
+                        help="Extension of input files from inputs-dir to test",
+                        type=list,
+                        default=[".rnd"],
+                        action="append")
+
+    # mutually exclusive arguments
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--inputs-dir", "-d",
+                       help="Directory containing input files. Specify either 'inputs-dir' or 'file'",
+                       type=str)
+    group.add_argument("--input-file", "-f",
+                       help="Path to a single input file to test. Specify either 'file' or 'inputs-dir'",
+                       type=str)
+
+    # optional battery skip
+    bats = ["bsi", "fips", "nist", "diehard", "tu01", "tu01-smallcrust", "tu01-crush",
+            "tu01-bigcrush", "tu01-rabbit", "tu01-alphabit", "tu01-blockalphabit"]
+    for bat in bats:
+        parser.add_argument(f"--no-{bat}",
+                            help=f"Skip tests from {bat.upper()} battery",
+                            action="store_true")
+
+    return parser.parse_args()
